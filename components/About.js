95fc88e7b79b488cd6823cbc3b339809fb712592
GameNotCreator <gnc.ytb@gmail.com>
2025-03-12 15:27:21 -0400

    pot

--- components/About.js
+++ components/About.js
@@ -12,24 +12,27 @@ const About = () => {
         <img src="/photo.jpg" alt="Hedi Fourati" className="profile-pic" />
         <div className="bio">
           <p>
-            <strong>Hedi Fourati</strong>
+            <strong>Hedi Fourati - CEO of TunisianPass</strong>
           </p>
           <p>Born on 15/01/2007 in La Marsa, Tunisia</p>
           <p>
-            I spent my entire childhood in La Marsa – from kindergarten, then
-            elementary school at Paul Verlaine School, followed by middle and
-            high school at Lycée Gustave Flaubert. I have many passions, but my
-            greatest is computing.
-          </p>
-          <p>
-            I started coding at the age of 8, entirely self-taught, and I love
-            to create, build, design, and learn. I also learned chess at a young
-            age and participated in several tournaments, even playing against
-            former world champion Anatoly Karpov during a simultaneous
-            exhibition.
+            My dream is to innovate beyond limits. I believe that the more I
+            learn, the more I can create. My goal is to satisfy my curiosity
+            through learning and to apply this knowledge to innovate in
+            technology. "I have many passions, but my greatest is computing. I
+            started coding at the age of 8, entirely self-taught, and I have
+            never stopped since. I constantly work on new projects, feeding my
+            insatiable curiosity and desire for innovation.
           </p>
           <p>Welcome to my portfolio!</p>
           <br />
+          <a className="contact2-button" target="_blank" href="/resume.pdf">
+            Open my resume
+          </a>
+          <br />
+          <br />
+          <br />
+
           <a className="contact-button" href="/#portfolio">
             Discover my projects
           </a>
@@ -112,6 +115,21 @@ const About = () => {
           .contact-button:hover {
             background: #f0f0f0;
             color: #1a1a1a;
+          }
+
+          .contact2-button {
+            background: white;
+            color: #1a1a1a;
+            border: 2px solid #1a1a1a;
+            padding: 0.75rem 1.5rem;
+            cursor: pointer;
+            transition: background 0.3s ease, color 0.3s ease;
+            font-family: "Helvetica Neue", sans-serif;
+            font-size: 1rem;
+          }
+          .contact2-button:hover {
+            background: transparent;
+            color: #f0f0f0;
           }
       `}</style>
     </motion.div>
